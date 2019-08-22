392788e4af7ad6099ecbc3ed4853dedc962e473c
denis_den <denis_gor@bk.ru>
2019-08-22 20:58:09 +0300

    15

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import s from './Navbar.module.css';
+console.log(s)
 
 // let s = {
-// 	'nav': 'Navbar_nav__DP0nY',
-// 	'item': 'Navbar_item__1LdOY'
+// 	nav: 'Navbar_nav__DP0nY',
+// 	item: 'Navbar_item__1LdOY',
+// 	active: 'Navbar_.......'
 // }
 
-let c1 = "item";
-let c2 = "active";
+// let c1 = "item";
+// let c2 = "active";
 // "item active"
 // let classes = с1 + " " + c2;
-let classes = `${c1} ${c2}`;
+// let classesNew = `${s.item} ${s.active}`;
 
 const Navbar = () => {
 	return(
 		<nav className={s.nav}>
-			<div className={s.item active}><a>Profile</a></div>
+			<div className={`${s.item} ${s.active}`}><a>Profile</a></div>  
 			<div className={s.item}><a>Messages</a></div>
 			<div className={s.item}><a>News</a></div>
 			<div className={s.item}><a>Music</a></div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
-	return(
+	return (
 		<div className={s.content}>
 			<div>
 				<img src="https://cdn.pixabay.com/photo/2017/02/08/17/24/butterfly-2049567__340.jpg" />
 			</div>
 
-			<div>My posts</div>
-
-			<div>
-				<div>New post</div>
-			</div>
-
-			<div className={s.posts}>
-				<div className={s.item}>post 1</div>
-				<div className={s.item}>post 2</div>
-			</div>
+			<MyPosts/> 
 
 		</div>
 	)
