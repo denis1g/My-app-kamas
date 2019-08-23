05d5efebca0af722b235ff9415472631b0330ffa
denis_den <denis_gor@bk.ru>
2019-08-23 10:06:18 +0300

    15.5

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Post.module.css';
+
+const Post = () => {
+	return (
+
+		<div className={s.item}>
+			<img src="https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg"/>
+			Post 1
+			<div>
+				<span>Like</span>
+			</div>
+		</div>
+
+	)
+}
+
+export default Post;
