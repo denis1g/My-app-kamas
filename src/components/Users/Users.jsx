018cf53dc4038d8c8c14a49e42f4dd4fd7bbed98
denis_den <denis_gor@bk.ru>
2019-09-12 20:46:25 +0300

    56

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,47 +1,58 @@
 import React from 'react';
-import styles from './users.module.css';
+import styles from "./users.module.css";
+import userPhoto from "../../assets/images/User.png";
 
-const Users = (props) => {
-	if(props.users.length === 0){
-		props.setUsers([
-			
-			{id: 1, photoUrl: 'https://m.media-amazon.com/images/M/MV5BN2VlNTVjZDItY2M5NC00ZTFjLWI1MDktOWFkMGIzN2IyOTI0XkEyXkFqcGdeQXVyNjMwOTA1MDM@._V1_UY317_CR130,0,214,317_AL_.jpg',
-				followed: false, fullName: 'Dmitry', status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'}},
-			{id: 2, photoUrl: 'https://m.media-amazon.com/images/M/MV5BN2VlNTVjZDItY2M5NC00ZTFjLWI1MDktOWFkMGIzN2IyOTI0XkEyXkFqcGdeQXVyNjMwOTA1MDM@._V1_UY317_CR130,0,214,317_AL_.jpg',
-				followed: true, fullName: 'Sasha', status: 'I am a boss too', location: {city: 'Moscow', country: 'Russia'}},
-			{id: 3, photoUrl: 'https://m.media-amazon.com/images/M/MV5BN2VlNTVjZDItY2M5NC00ZTFjLWI1MDktOWFkMGIzN2IyOTI0XkEyXkFqcGdeQXVyNjMwOTA1MDM@._V1_UY317_CR130,0,214,317_AL_.jpg',
-				followed: false, fullName: 'Andrew', status: 'I am a boss too', location: {city: 'Kiev', country: 'Ukraine'}},
-		
-		])
+let Users = (props) => {
+	
+	let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+	
+	let pages = [];
+	for(let i = 1; i <= pagesCount; i++){
+		pages.push(i);
 	}
 	
 	return <div>
+		<div>
+			{pages.map(p => {
+				return <span className={props.currentPage === p && styles.selectedPage}
+				             onClick={(e) => {
+					             props.onPageChanged(p)
+				             }}>{p}</span>
+			})}
+		
+		</div>
+		{/*<button onClick={getUsers}>Get Users</button>*/}
 		{
 			props.users.map(u => <div key={u.id}>
 					<span>
 						<div>
-							<img src={u.photoUrl} className={styles.userPhoto}/>
+							<img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
 						</div>
 						<div>
 							{u.followed ?
-								<button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> :
-								<button onClick={() => {props.follow(u.id)}}>Follow</button>
+								<button onClick={() => {
+									props.unfollow(u.id)
+								}}>Unfollow</button> :
+								<button onClick={() => {
+									props.follow(u.id)
+								}}>Follow</button>
 							}
 							
 						</div>
 					</span>
 				<span>
 							<span>
-								<div>{u.fullName}</div>
+								<div>{u.name}</div>
 								<div>{u.status}</div>
 							</span>
 							<span>
-								<div>{u.location.country}</div>
-								<div>{u.location.city}</div>
+								<div>{"u.location.country"}</div>
+								<div>{"u.location.city"}</div>
 							</span>
 					</span>
 			</div>)
 		}
 	</div>
 }
+
 export default Users;
